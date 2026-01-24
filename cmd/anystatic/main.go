5f5343dab453d7650f6ae271c8a6d312ff64f5c9
Watanabe Takashi <wtnb75@gmail.com>
2026-01-24 09:48:34 +0900

    unix domain socket (#6)

--- cmd/anystatic/main.go
+++ cmd/anystatic/main.go
@@ -4,11 +4,23 @@ import (
 	"flag"
 	"io/fs"
 	"log/slog"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"strings"
 
 	"github.com/wtnb75/anystatic"
 )
+
+func do_listen(listen string) (net.Listener, error) {
+	protos := strings.SplitN(listen, ":", 2)
+	switch protos[0] {
+	case "unix", "tcp", "tcp4", "tcp6":
+		return net.Listen(protos[0], protos[1])
+	}
+	return net.Listen("tcp", listen)
+}
 
 func realMain() error {
 	listen := flag.String("listen", ":8800", "listen address")
@@ -25,11 +37,23 @@ func realMain() error {
 	fs := os.DirFS(*dir).(fs.StatFS)
 	hdl := anystatic.NewHandler(fs)
 	server := http.Server{
-		Addr:    *listen,
 		Handler: hdl,
 	}
+	listener, err := do_listen(*listen)
+	if err != nil {
+		slog.Error("listen error", "error", err)
+		return err
+	}
+	defer listener.Close()
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		<-c
+		slog.Info("shutting down server")
+		listener.Close()
+	}()
 	slog.Info("starting server", "addr", server.Addr)
-	return server.ListenAndServe()
+	return server.Serve(listener)
 }
 
 func main() {
